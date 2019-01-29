688b489f1ec667c06f3419705f8409f503b8720f
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-01-28 20:59:55 -0800

    added check answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 var categoryList = {
     "Film": 11,
     "Music": 12,
-    "Televsion": 14,
+    "Television": 14,
     "Video Games": 15,
     "Sports": 21
 }
@@ -18,6 +18,7 @@ var $choices = $('.choices');
 var str = "";
 var queryURL = "";
 var currentQuestion = 0;
+var correctAnswer = "";
 
 $('document').ready(function () {
     window.onload = function () {
@@ -39,7 +40,8 @@ function questionSearch() {
                     data.results[i].incorrect_answers[0],
                     data.results[i].incorrect_answers[1],
                     data.results[i].incorrect_answers[2],
-                ]
+                ],
+                correctAnswer : data.results[i].correct_answer
             })
         }
     })
@@ -56,6 +58,7 @@ $categoryAll.on('click', function () {
 function timerCountdown() {
     if (time > 0) {
         time--
+        console.log(time);
         $('#timer').text(time);
     }
     else if (time == 0) {
@@ -80,12 +83,21 @@ function shuffle(a) {
 $('#start').on('click', function () {
     nextQuestion();
     $(this).fadeOut('slow');
-    setInterval(timerCountdown, 500);
+    setInterval(timerCountdown, 1000);
+});
+
+$('.choices').on('click',function(){
+    if($(this).text() == correctAnswer){
+        alert("CORRECT");
+    }else{
+        alert("INCORRECT");
+    }
 });
 
 function nextQuestion() {
     $('#mainDisplay').fadeOut('slow',function(){
         var questionOrder = shuffle([0,1,2,3]);
+        correctAnswer = gameData[currentQuestion].correctAnswer;
         $question.html(gameData[currentQuestion].question);
         $choices.eq(0).html(gameData[currentQuestion].answers[questionOrder[0]]);
         $choices.eq(1).html(gameData[currentQuestion].answers[questionOrder[1]]);
@@ -94,36 +106,4 @@ function nextQuestion() {
         currentQuestion++;
         $(this).fadeIn('slow');
     })
-}
-
-
-
-
-
-
-
-
-
-
-
-// function questionSearch() {
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (data) {
-//         // debugger;
-
-//         setInterval(timerCountdown, 1000);
-//         console.log(time);
-//         while (currentQuestion < 10) {
-//             // debugger;
-//             $question.html(data.results[currentQuestion].question);
-//             $choices.eq(0).html(data.results[currentQuestion].correct_answer);
-//             $choices.eq(1).html(data.results[currentQuestion].incorrect_answers[0]);
-//             // debugger;
-//             $choices.eq(2).html(data.results[currentQuestion].incorrect_answers[1]);
-//             $choices.eq(3).html(data.results[currentQuestion].incorrect_answers[2]);
-//             currentQuestion++;
-//         }
-//     })
-// };
+};
