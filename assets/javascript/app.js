c0f05c572d989c194dea81788be07e55568b1c04
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-01-28 19:36:15 -0800

    Questioned change on time=0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,81 +25,97 @@ $('document').ready(function () {
     }
 });
 
+function questionSearch() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (data) {
+        console.log(data.results);
+        for (var i = 0; i < ((data.results).length); i++) {
+            gameData.push({
+                question: data.results[i].question,
+                answers:[
+                    data.results[i].correct_answer,
+                    data.results[i].incorrect_answers[0],
+                    data.results[i].incorrect_answers[1],
+                    data.results[i].incorrect_answers[2],
+                ]
+            })
+        }
+    })
+}
+
 $categoryAll.on('click', function () {
     // debugger;
     category = $(this).text();
     queryURL = "https://opentdb.com/api.php?amount=10&category=" + categoryList[category] + "&difficulty=medium&type=multiple";
     $('#start').fadeIn('slow');
     $('.category').fadeOut('slow');
+    questionSearch();
 });
 
 function timerCountdown() {
     if (time > 0) {
         time--
         console.log(time);
+        $('#timer').text(time);
     }
-    $('#timer').text(time);
+    else if (time == 0) {
+        time = 20;
+        $('#timer').text(time);
+        nextQuestion();
+    }
 }
-
-function questionSearch() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (data) {
-        // debugger;
-        
-        setInterval(timerCountdown, 1000);
-        console.log(time);
-        while (currentQuestion < 10) {
-            // debugger;
-            $question.html(data.results[currentQuestion].question);
-            $choices.eq(0).html(data.results[currentQuestion].correct_answer);
-            $choices.eq(1).html(data.results[currentQuestion].incorrect_answers[0]);
-            // debugger;
-            $choices.eq(2).html(data.results[currentQuestion].incorrect_answers[1]);
-            $choices.eq(3).html(data.results[currentQuestion].incorrect_answers[2]);
-            currentQuestion++;
-        }
-    })
-};
-
 
 //---------------Start Button-----------------//
 $('#start').on('click', function () {
     // debugger;
-    $(this).fadeOut('slow',questionSearch());
+    nextQuestion();
+    $(this).fadeOut('slow');
+    setInterval(timerCountdown, 500);
 });
 
+function nextQuestion() {
+    $('#mainDisplay').fadeOut('slow',function(){
+        $question.html(gameData[currentQuestion].question);
+        $choices.eq(0).html(gameData[currentQuestion].answers[0]);
+        $choices.eq(1).html(gameData[currentQuestion].answers[1]);
+        // debugger;
+        $choices.eq(2).html(gameData[currentQuestion].answers[2]);
+        $choices.eq(3).html(gameData[currentQuestion].answers[3]);
+        currentQuestion++;
+        $(this).fadeIn('slow');
+    })
+}
 
 
-// function questionSearch(){
+
+
+
+
+
+
+
+
+
+// function questionSearch() {
 //     $.ajax({
 //         url: queryURL,
 //         method: "GET"
 //     }).then(function (data) {
-//         console.log(data.results);
-//         for(var i =0; i<((data.results).length); i++){
+//         // debugger;
 
-//             gameData.push({
-//                 question : data.results[i].question,
-//                 correct : data.results[i].correct_answer,
-//                 incorrect1 : data.results[i].incorrect_answers[0],
-//                 incorrect2 : data.results[i].incorrect_answers[1],
-//                 incorrect3 : data.results[i].incorrect_answers[2],
-//             })
-//         }
-
-//         while(currentQuestion<10){
-//             $question.html(gameData[currentQuestion].question);
-//             $choices.eq(0).html(gameData[currentQuestion].correct);
-//             $choices.eq(1).html(gameData[currentQuestion].incorrect1);
-//             $choices.eq(2).html(gameData[currentQuestion].incorrect2);
-//             $choices.eq(3).html(gameData[currentQuestion].incorrect3);
+//         setInterval(timerCountdown, 1000);
+//         console.log(time);
+//         while (currentQuestion < 10) {
+//             // debugger;
+//             $question.html(data.results[currentQuestion].question);
+//             $choices.eq(0).html(data.results[currentQuestion].correct_answer);
+//             $choices.eq(1).html(data.results[currentQuestion].incorrect_answers[0]);
+//             // debugger;
+//             $choices.eq(2).html(data.results[currentQuestion].incorrect_answers[1]);
+//             $choices.eq(3).html(data.results[currentQuestion].incorrect_answers[2]);
 //             currentQuestion++;
-//             time = 20;
-//             while(time>0){
-//                 setInterval(timerCountdown, 500);
-//             }
 //         }
 //     })
-// }
+// };
